803d3aa4136fe0d5854d5fcea68b5abe86f773b3
GoatedChopin <85979611+GoatedChopin@users.noreply.github.com>
2022-10-12 12:11:43 -0500

    Add files via upload
    Added binary_search to Search.py

--- Algorithms/Search.py
+++ Algorithms/Search.py
@@ -53,3 +53,27 @@ def dijkstra(start_node, end_node):
                 node.total_distance = min(node.total_distance, current_node.distance(node) + total_distance)
                 heap.push((node.total_distance, node))
     return None
+
+
+def binary_search(sorted_iterable, target):
+    """
+    Returns the index of the target value in sorted_iterable,
+    if target is not in sorted_iterable, returns None
+    """
+    n = len(sorted_iterable)
+    l, r = 0, n-1
+    while l <= r:
+        m = (l + r) // 2
+        current = sorted_iterable[m]
+        if current == target:
+            return m
+        elif current < target:
+            l = m + 1
+        else:
+            r = m - 1
+    return -1
+
+
+if __name__ == "__main__":
+    assert binary_search([1,2,3,4,5], 3) == 2
+    assert binary_search([1,2,3,4,5], 7) == -1
