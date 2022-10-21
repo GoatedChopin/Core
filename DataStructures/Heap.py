86f4106cf5d58c529eba1c16b938226a8a195996
Colby Meline <85979611+GoatedChopin@users.noreply.github.com>
2022-10-21 17:08:30 -0500

    Update to heap, binary tree based class

--- DataStructures/Heap.py
+++ DataStructures/Heap.py
@@ -1,7 +1,7 @@
 from heapq import heappush, heappop, heappushpop, heapify
 
 
-class Heap:
+class LazyHeap:
     """
     Lazy implementation of a heap using heapq, will replace with the full binary tree logic soon(ish).
     """
@@ -20,3 +20,36 @@ class Heap:
 
     def pushpop(self, val):
         return heappushpop(self.elements, val*self.augment)*self.augment
+
+
+class Heap:
+    """
+    Full implementation of a heap using a binary tree,
+    all children must be less than or equal to their parents.
+    """
+    class HeapNode:
+        def __init__(self, val=None):
+            self.val = None
+            self.left = None
+            self.right = None
+
+    def __init__(self, val=None):
+        self.head = self.HeapNode(val=val)
+
+    def getMax(self):
+        return self.head.val
+
+    def push(self, val):
+        pass
+
+    def pop(self):
+        pass
+
+    def heapify(self):
+        pass
+
+    def siftDown(self):
+        pass
+
+    def siftUp(self):
+        pass
