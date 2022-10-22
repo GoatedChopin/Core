931dac1cf12f1e15ce5e4ce70ef69ecab165e47b
Colby Meline <85979611+GoatedChopin@users.noreply.github.com>
2022-10-21 20:55:39 -0500

    Fixed -1 index bug
    Heap class is now operational, MaxTreeHeap is hopeless

--- DataStructures/Heap.py
+++ DataStructures/Heap.py
@@ -1,4 +1,55 @@
 from heapq import heappush, heappop, heappushpop, heapify
+from collections import deque
+
+
+class Heap:
+    """
+    Please don't use any of the other heaps in this file, they're embarrassing.
+    The tree one is broken and not worth fixing (it's much more efficient to use an array to back the heap anyway).
+    """
+    def __init__(self):
+        self.elements = []
+        self.size = 0
+
+    def push(self, val):
+        self.size += 1
+        self.elements.append(val)
+        self.siftUp(self.size-1)
+
+    def pop(self):
+        if self.size > 0:
+            self.size -= 1
+            out_val = self.elements[0]
+            self.elements[0] = self.elements[-1]
+            self.elements.pop()
+            self.siftDown(0)
+            return out_val
+
+    def siftDown(self, parent):
+        largest = parent
+        leftChild = 2 * parent + 1
+        rightChild = 2 * parent + 2
+
+        if leftChild < self.size and self.elements[leftChild] > self.elements[largest]:
+            largest = leftChild
+        if rightChild < self.size and self.elements[rightChild] > self.elements[largest]:
+            largest = rightChild
+
+        if largest != parent:
+            self.swap(largest, parent)
+            self.siftDown(largest)
+
+    def siftUp(self, index):
+        if index == 0:
+            return
+        parent = (index - 1) // 2
+        print("Parent is {}".format(parent))
+        if self.elements[parent] < self.elements[index]:
+            self.swap(parent, index)
+            self.siftUp(parent)
+
+    def swap(self, index_a, index_b):
+        self.elements[index_a], self.elements[index_b] = self.elements[index_b], self.elements[index_a]
 
 
 class LazyHeap:
@@ -22,34 +73,117 @@ class LazyHeap:
         return heappushpop(self.elements, val*self.augment)*self.augment
 
 
-class Heap:
+class MaxTreeHeap:
     """
     Full implementation of a heap using a binary tree,
     all children must be less than or equal to their parents.
     """
-    class HeapNode:
-        def __init__(self, val=None):
-            self.val = None
-            self.left = None
-            self.right = None
-
-    def __init__(self, val=None):
-        self.head = self.HeapNode(val=val)
+    def __init__(self, val=None, parent=None):
+        self.val = val
+        self.parent = None
+        self.left = None
+        self.right = None
 
     def getMax(self):
         return self.head.val
 
     def push(self, val):
-        pass
+        node = self.first_empty_node()
+        node.val = val
+        node.siftUp()
 
     def pop(self):
-        pass
-
-    def heapify(self):
-        pass
+        return_val = self.val
+        self.val = -float("inf")
+        node = self.siftDown()
+        del node
+        return return_val
 
     def siftDown(self):
-        pass
+        out = self
+        if self.left is not None and self.left.val > self.val:
+            largest = self.left
+        elif self.right is not None and self.right.val > self.val:
+            largest = self.right
+        else:
+            largest = self
+
+        if largest != self:
+            # print("Swapping {} with {}".format(largest.val, self.val))
+            self.swap(largest)
+            out = largest.siftDown()
+        return out
 
     def siftUp(self):
-        pass
+        if self.parent is not None and self.parent.val < self.val:
+            self.swap(self.parent)
+            self.parent.siftUp()
+
+    def swap(self, other):
+        self.val, other.val = other.val, self.val
+
+    def bfs(self, val):
+        if self.val == val:
+            return self
+        queue = deque()
+        if self.left is not None:
+            queue.appendleft(self.left)
+        if self.right is not None:
+            queue.appendleft(self.right)
+        while len(queue) > 0:
+            current = queue.pop()
+            if current.val == val:
+                return current
+            if current.left is not None:
+                queue.appendleft(current.left)
+            if current.right is not None:
+                queue.appendleft(current.right)
+        return None
+
+    def first_empty_node(self):
+        """
+        O(n) time complexity to find empty spot, very bad for a heap,
+        which is supposed to have O(log(n)) time complexity for push / pop
+        """
+        if self.val is None:
+            return self
+        queue = deque()
+        if self.left is not None:
+            queue.appendleft(self.left)
+        else:
+            self.left = MaxTreeHeap()
+            return self.left
+        if self.right is not None:
+            queue.appendleft(self.right)
+        else:
+            self.right = MaxTreeHeap()
+            return self.right
+
+        while len(queue) > 0:
+            current = queue.pop()
+            if current.left is None:
+                current.left = MaxTreeHeap()
+                current.left.parent = current
+                return current.left
+            else:
+                queue.appendleft(current.left)
+
+            if current.right is None:
+                current.right = MaxTreeHeap()
+                current.right.parent = current
+                return current.right
+            else:
+                queue.appendleft(current.right)
+
+
+if __name__ == "__main__":
+    import random as r
+    heap = Heap()
+    for i in range(1000):
+        heap.push(r.randint(0, 1000))
+    current = 1001
+    for i in range(1000):
+        next_val = heap.pop()
+        print(next_val)
+        assert current >= next_val
+        current = next_val
